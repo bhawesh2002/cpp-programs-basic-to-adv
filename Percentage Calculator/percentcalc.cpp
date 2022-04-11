7a39a1f752087e418abc66961727a96a0c0c3edc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-11 16:50:53 +0530

    getting input from user

--- Percentage Calculator/percentcalc.cpp
+++ Percentage Calculator/percentcalc.cpp
@@ -5,6 +5,8 @@ using namespace std;        //we are using the standard namespace(simply,no need
 int main(){                 //main function, most important function in a program
     int m1,m2,m3,m4,m5;     //declaring varuiable to hold makrs of students
     int total = 0;          //variable to hold total marks, initializing it to 0 to get rid of any junk values
+    cout << "Please Enter Your Marks(out fo 100): "; //prompting user to enter marks
+    cin >> m1 >> m2 >> m3 >> m4 >> m5;           //accepting marks from user
     total = m1 + m2 + m3 + m4 + m5; //calculating total marks
     int percent = 0;        //variable to hold percentage, initializing it to 0 to get rid of any junk values
     percent = (total/500)*100;  //calculating percentage using formaula
