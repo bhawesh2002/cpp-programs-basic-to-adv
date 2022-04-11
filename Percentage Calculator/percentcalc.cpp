71790ef347795c11007f785782c1f04302676e23
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-11 16:35:24 +0530

    add general structure of any cpp program

---
+++ Percentage Calculator/percentcalc.cpp
@@ -0,0 +1,7 @@
+#include <iostream>         //preprocessor directive, instructs the preprocessor to include the iostream library
+
+using namespace std;        //we are using the standard namespace(simply,no need to use std::cout,use cout instead)
+
+int main(){                 //main function, most important function in a program
+    //contains our code
+}
