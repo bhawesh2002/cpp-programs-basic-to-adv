b257a4f0aaee4374ad0e0a770657f51ae288ef61
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-12 14:12:10 +0530

    find solution to the question

---
+++ numerator and denominator/num_and_deno.cpp
@@ -0,0 +1,5 @@
+/*Write a program which asks the user to enter three fractions. Using the three fractions
+inputted by the user, find the resultant sum in fractional
+form and display it on the screen.
+Hint: Use cascading of operators to simplify program*/
+
