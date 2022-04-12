0af7312167efc382e1e97bb438aa2b259933c2ef
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-12 13:56:15 +0530

    commenting the code

--- AreaOfCircle/AOCircle.cpp
+++ AreaOfCircle/AOCircle.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
+#include <iostream> //preprocessor directive, instructs the preprocessor to include the iostream library
 
-#define PI 3.14;
+#define PI 3.14; // define PI as 3.14 (PI is a constant, can be used anywhere in the program)
 
-using namespace std;
+using namespace std; // we are using the standard namespace(simply,no need to use std::cout,use cout instead)
 
-int main(){
-    float radius{0};
-    cout << "Enter the radius of the circle: ";
-    cin >> radius;
-    float area = (radius * radius) * PI;
-    cout << "The area of the circle is: " << area << endl;
-    return 0;
+int main()
+{                                                          // main function, most important function in a program
+    float radius{0};                                       // using brace initialization to initialize radius to 0
+    cout << "Enter the radius of the circle: ";            // prompting user to enter radius
+    cin >> radius;                                         // accepting radius from user
+    float area = (radius * radius) * PI;                   // calculating area using formaula
+    cout << "The area of the circle is: " << area << endl; // printing area
+    return 0;                                              // returning 0 to the operating system
 }
