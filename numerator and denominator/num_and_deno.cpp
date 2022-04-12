8e80f3f1fa6e3dd650d58068256bbfbfcd116597
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-12 14:20:26 +0530

    commenting the code

--- numerator and denominator/num_and_deno.cpp
+++ numerator and denominator/num_and_deno.cpp
@@ -3,13 +3,14 @@ inputted by the user, find the resultant sum in fractional
 form and display it on the screen.
 Hint: Use cascading of operators to simplify program*/
 
-#include <iostream>
+#include <iostream> // for standard I/O(more simply cin and cout)
 
-using namespace std;
+using namespace std; // namespace std is used to reduce the use of std::
 
-int main(){
+int main()
+{ // main function
 
-    int num1,num2,num3,den1,den2,den3;
+    int num1, num2, num3, den1, den2, den3; // declaring variables
     cout << "Enter the numerator and denominator of the first fraction: " << endl;
     cin >> num1 >> den1;
     cout << "Enter the numerator and denominator of the second fraction: " << endl;
@@ -17,10 +18,10 @@ int main(){
     cout << "Enter the numerator and denominator of the third fraction: " << endl;
     cin >> num3 >> den3;
 
-    int num_sum = num1*den2 + num2*den3 + num3*den1;
-    int den_sum = den1*den2*den3;
+    int num_sum = num1 * den2 + num2 * den3 + num3 * den1; // calculating the numerator of the resultant sum
+    int den_sum = den1 * den2 * den3;                      // calculating the denominator of the resultant sum
 
-    cout << "The sum of the fractions is: " << num_sum << "/" << den_sum << endl;
+    cout << "The sum of the fractions is: " << num_sum << "/" << den_sum << endl; // displaying the resultant sum
 
     return 0;
 }
