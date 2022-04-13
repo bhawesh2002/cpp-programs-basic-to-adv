a977f70f619f0721d0bbf398603d8f36865ea52d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-13 19:58:10 +0530

    source code to print sum of odd series

---
+++ 5.SumOfOdds/SOOdds.cpp
@@ -0,0 +1,22 @@
+/*Write a program to find the sum of the series 1 + 3 + 5 + 7……n terms. The value of ‘n’
+is given by the user during program execution?*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, sum = 0; // declare variables n and sum; initialize sum to 0(only sum is initialised to 0 not n)
+    cout << "Enter the value of n: ";
+    cin >> n;
+    int i = 1;
+    while (n * 2 >= i)  // while loop to iterate through the series
+    {
+        cout << i << " + "; // prints the value of i
+        sum = sum + i; // adds the value of i to sum
+        i = i + 2;     // increments i by 2
+    }
+    cout << "\nThe sum of the series is: " << sum << endl;
+    return 0;
+}
