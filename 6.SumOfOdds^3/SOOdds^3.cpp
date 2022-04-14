54ede4418c5812b47f7a1d82f657fbab0ea75d7e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-14 14:32:32 +0530

    commenting the code

--- 6.SumOfOdds^3/SOOdds^3.cpp
+++ 6.SumOfOdds^3/SOOdds^3.cpp
@@ -1,19 +1,21 @@
 /*Write a program to find the sum of the series 1^3 + 3^3 + 5^3 + 7^3 + … n terms. Assume
 that the value of n is given by the user during program execution?*/
 
-#include <iostream>
+#include <iostream> // for basic I/O(cin, cout)
 
-using namespace std;
+using namespace std; // using namespace std;
 
-int main(){
+int main()
+{
     int n, sum = 0, i = 1;
     cout << "Enter the value of n: ";
     cin >> n;
-    while (n*2 >= i){
-        cout << i << "^3 + "; 
-        sum += i*i*i;
-        i += 2;
+    while (n * 2 >= i)
+    {
+        cout << i << "^3 + "; // print the series
+        sum += i * i * i;     // calculate the sum(note there is only change in formula from previous code)
+        i += 2;               // increment i by 2
     }
-    cout << "\nSum = " << sum << endl; 
+    cout << "\nSum = " << sum << endl; // print the sum
     return 0;
 }
