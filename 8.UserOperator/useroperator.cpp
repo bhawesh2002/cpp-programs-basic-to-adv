7b0566b3319b184163c54da64066c263fca37e80
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-14 15:10:26 +0530

    commenting the code

--- 8.UserOperator/useroperator.cpp
+++ 8.UserOperator/useroperator.cpp
@@ -2,37 +2,38 @@
 operator (+,-,/,*,%) perform the desired operation and print the result
 (use switch case) ?*/
 
-#include <iostream>
+#include <iostream> // for standard I/O(for cin and cout)
 
 using namespace std;
 
-int main(){
-    int a, b;
+int main()
+{             // main function
+    int a, b; // declaring variables
     cout << "Enter the first number: ";
     cin >> a;
     cout << "\nEnter the second number: ";
     cin >> b;
-    char op;
+    char op; // declaring variable for storing user entered operator
     cout << "\nEnter the operator (+,-,/,*,%): ";
-    cin >> op;
-    switch (op)
+    cin >> op;  // prompting user to enter an operator
+    switch (op) // switch case: execudes code based on the case entered
     {
-    case '+' :
+    case '+': // if case is +
         cout << "Sum of " << a << " and " << b << " is " << a + b;
         break;
-    case '-' :
+    case '-': // if case is -
         cout << "Difference of " << a << " and " << b << " is " << a - b;
         break;
-    case '*' :
+    case '*': // if case is *
         cout << "Product of " << a << " and " << b << " is " << a * b;
         break;
-    case '/' :
+    case '/': // if case is /
         cout << "Quotient of " << a << " and " << b << " is " << a / b;
         break;
-    case '%' :
+    case '%': // if case is %
         cout << "Remainder of " << a << " and " << b << " is " << a % b;
         break;
-    default:
+    default: // if case is not +,-,*,/,%
         cout << "Invalid operator";
         break;
     }
