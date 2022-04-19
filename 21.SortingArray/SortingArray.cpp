e6ec7bf3d573d8c36b8084b192e3aa6cb8cd35f4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-19 11:26:46 +0530

    comment the code

--- 21.SortingArray/SortingArray.cpp
+++ 21.SortingArray/SortingArray.cpp
@@ -6,23 +6,30 @@ int main()
 {
     int numbers[5] = {0};
     cout << "Fill the array with 5 numbers" << endl;
-    for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
+    for (int i = 0; i < sizeof(numbers) / sizeof(int); i++) //loop to fill the array
     {
         cout << i + 1 << ".";
         cin >> numbers[i];
     }
+    //sort array in ascending order
     for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
     {
-        if(numbers[i] > numbers[i+1]){
+        if (numbers[i] > numbers[i + 1])
+        {
             int temp = numbers[i];
-            numbers[i] = numbers[i+1];
-            numbers[i+1] = temp; 
+            numbers[i] = numbers[i + 1];
+            numbers[i + 1] = temp;
         }
     }
+
+printArray:
+{
     cout << "The sorted array is" << endl;
     for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
     {
+
         cout << i + 1 << "." << numbers[i] << endl;
     }
+}
     return 0;
 }
