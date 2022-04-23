3e10ff580a2d68db288e2ceae4c588c0b38eb801
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-23 11:43:57 +0530

    creation of library struct

---
+++ 24.BasicLibraryManageSystem/LibMagSys.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>  //for strcmp(): function used to compare strings
+#include <cstdio>  //for gets(): function used to get user input
+using namespace std;
+
+struct library
+{
+    char book_name[25];
+    char author[25];
+    int shelf_number;
+    int price;
+};
+
+int main()
+{
+    return 0;
+}
