5417afb6174503a51b33a85ea0e1eb6ce45bf75e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-23 11:44:38 +0530

    display  the options

--- 24.BasicLibraryManageSystem/LibMagSys.cpp
+++ 24.BasicLibraryManageSystem/LibMagSys.cpp
@@ -13,5 +13,26 @@ struct library
 
 int main()
 {
+    
+    cout << "BASIC LIBRAY MANAGEMENT SYSTEM" << endl;
+    cout << "==============================" << endl;
+    cout << "1. Add a new book" << endl;
+    cout << "2. Remove a book" << endl;
+    cout << "3. Search for a book" << endl;
+    cout << "4. Display all books" << endl;
+    cout << "5. Exit" << endl;
+    cout <<"===============================" << endl;
+    cout << "Enter your choice: ";
+    int choice;
+    cin >> choice;
+    switch (choice)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
     return 0;
 }
