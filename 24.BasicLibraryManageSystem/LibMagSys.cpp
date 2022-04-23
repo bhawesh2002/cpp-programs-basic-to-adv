cb057fe751e9e4310058abd5d12325f491e2a8a8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-04-23 13:32:58 +0530

    Add code to Quit the program

--- 24.BasicLibraryManageSystem/LibMagSys.cpp
+++ 24.BasicLibraryManageSystem/LibMagSys.cpp
@@ -13,109 +13,117 @@ struct library
 int main()
 {
     library book[5];
-    cout << "BASIC LIBRAY MANAGEMENT SYSTEM" << endl;
-    cout << "==============================" << endl;
-    cout << "1. Add a new book(You can add upto " << sizeof(book) / sizeof(library) << " books)" << endl;
-    cout << "2. Remove a book" << endl;
-    cout << "3. Search for a book" << endl;
-    cout << "4. Display all books" << endl;
-    cout << "5. Exit" << endl;
-    cout << "===============================" << endl;
-    cout << "Enter your choice: ";
-    int choice;
-    int i = 1; // to store the number of books added
-    cin >> choice;
-    switch (choice)
+    int choice; //for user choice
+    do
     {
-    case 1:
-    {
-        cout << "Enter the details of book you wnat to add: " << endl;
-        while (i <= 5)
+        cout << "BASIC LIBRAY MANAGEMENT SYSTEM" << endl;
+        cout << "==============================" << endl;
+        cout << "1. Add a new book(You can add upto " << sizeof(book) / sizeof(library) << " books)" << endl;
+        cout << "2. Remove a book" << endl;
+        cout << "3. Search for a book" << endl;
+        cout << "4. Display all books" << endl;
+        cout << "5. Exit" << endl;
+        cout << "===============================" << endl;
+        cout << "Enter your choice: ";
+        int i = 1; // to store the number of books added
+        cin >> choice;
+        switch (choice)
         {
-            cout << "Book name: ";
-            cin.getline(book[i].book_name, 25);
-            cout << "Author: ";
-            cin.getline(book[i].author, 25);
-            cout << "Shelf number: ";
-            cin >> book[i].shelf_number;
-            cout << "Price: ";
-            cin >> book[i].price;
-            cout << "Wanna Add More Books?(y/n): ";
-            char ch;
-            cin >> ch;
-            if (ch == 'y')
+        case 1:
+        {
+            cout << "Enter the details of book you wnat to add: " << endl;
+            while (i <= 5)
             {
-                cout << "Enter the details of book you wnat to add: " << endl;
-            }
-            else
-            {
-                break;
+                cout << "Book name: ";
+                cin.getline(book[i].book_name, 25);
+                cout << "Author: ";
+                cin.getline(book[i].author, 25);
+                cout << "Shelf number: ";
+                cin >> book[i].shelf_number;
+                cout << "Price: ";
+                cin >> book[i].price;
+                cout << "Wanna Add More Books?(y/n): ";
+                char ch;
+                cin >> ch;
+                if (ch == 'y')
+                {
+                    cout << "Enter the details of book you wnat to add: " << endl;
+                }
+                else
+                {
+                    break;
+                }
             }
         }
-    }
-    break;
-    case 2:
-    {
-        cout << "Enter the name & shelf of book you want to remove" << endl;
-        char book_name[25];
-        int shelf_number;
-        cout << "Enter the name of book: ";
-        cin.getline(book_name, 25);
-        cout << "Enter the shelf number: ";
-        cin >> shelf_number;
-        for (int i = 0; i <= 5; i++)
+        break;
+        case 2:
         {
-            if (book[i].shelf_number == shelf_number && strcmp(book[i].book_name, book_name) == 0)
+            cout << "Enter the name & shelf of book you want to remove" << endl;
+            char book_name[25];
+            int shelf_number;
+            cout << "Enter the name of book: ";
+            cin.getline(book_name, 25);
+            cout << "Enter the shelf number: ";
+            cin >> shelf_number;
+            for (int i = 0; i <= 5; i++)
             {
-                cout << "Book removed successfully" << endl;
-                i--; // to remove the book from the array
-                break;
-            }
-            else
-            {
-                cout << "Book not found" << endl;
+                if (book[i].shelf_number == shelf_number && strcmp(book[i].book_name, book_name) == 0)
+                {
+                    cout << "Book removed successfully" << endl;
+                    i--; // to remove the book from the array
+                    break;
+                }
+                else
+                {
+                    cout << "Book not found" << endl;
+                }
             }
         }
-    }
-    break;
-    case 3: // search for a book
-    {
-        cout << "Enter the name of book: ";
-        char book_name[25];
-        cin.getline(book_name, 25);
-        for (int i = 0; i <= 5; i++)
+        break;
+        case 3: // search for a book
         {
-            if (strcmp(book[i].book_name, book_name) == 0)
+            cout << "Enter the name of book: ";
+            char book_name[25];
+            cin.getline(book_name, 25);
+            for (int i = 0; i <= 5; i++)
             {
-                cout << "Book found" << endl;
+                if (strcmp(book[i].book_name, book_name) == 0)
+                {
+                    cout << "Book found" << endl;
+                    cout << "Book name: " << book[i].book_name << endl;
+                    cout << "Author: " << book[i].author << endl;
+                    cout << "Shelf number: " << book[i].shelf_number << endl;
+                    cout << "Price: " << book[i].price << endl;
+                    break;
+                }
+                else
+                {
+                    cout << "Book not found" << endl;
+                }
+            }
+        }
+        break;
+        case 4:
+        {
+            cout << "====Displaying All Books====" << endl;
+            for (int i = 0; i <= 5; i++)
+            {
+                cout << "Book No: " << i + 1 << endl;
                 cout << "Book name: " << book[i].book_name << endl;
                 cout << "Author: " << book[i].author << endl;
                 cout << "Shelf number: " << book[i].shelf_number << endl;
                 cout << "Price: " << book[i].price << endl;
-                break;
-            }
-            else
-            {
-                cout << "Book not found" << endl;
+                cout << "============================" << endl;
             }
         }
-    }
-    break;
-    case 4:
-    {
-        cout << "====Displaying All Books====" << endl;
-        for (int i = 0; i <= 5; i++)
-        {
-            cout << "Book No: " << i + 1 << endl;
-            cout << "Book name: " << book[i].book_name << endl;
-            cout << "Author: " << book[i].author << endl;
-            cout << "Shelf number: " << book[i].shelf_number << endl;
-            cout << "Price: " << book[i].price << endl;
-            cout << "============================" << endl;
-        }
-    }
-    default:
         break;
-    }
+        default:
+        {
+            cout << "Invalid Choice" << endl;
+            break;
+        }
+        }
+    } while (choice != 5);
+    cout << "*******Thank You for Visiting*******" << endl;
     return 0;
 }
