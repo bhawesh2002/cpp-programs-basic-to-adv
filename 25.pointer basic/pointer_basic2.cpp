05e39be3dd00c8b68fafda36434a25b4f5d78502
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-13 22:14:50 +0530

    code

--- 25.pointer basic/pointer_basic2.cpp
+++ 25.pointer basic/pointer_basic2.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main()
 {
-    
+    int amt = 1000;                                     // amount of money in bank
+    int *p;                                             // pointer to integer
+    p = &amt;                                           // assign address of amt to p
+    amt += 100;                                         // add 100 to amt
+    cout << "Amount of money in bank: " << *p << endl;  // print value stored in p
+    *p -= 2000;                                         // assign value 2000 to p
+    cout << "Amount of money in bank: " << *p << endl;  // print value stored in p
+    cout << "Amount of money in bank: " << amt << endl; // print value stored in amt
+    cout << "Address of amt: " << &amt << endl;         // print address of amt
+    cout << "Address of p: " << p << endl;              // print address of p
+    //both p and amt are pointing to same address
+    return 0;
 }
