7e184fdce96ff1625ec75728f9d8e2dbf3fa9d11
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-13 22:29:42 +0530

    comment the code

--- 26.sizeof()/sizeof.cpp
+++ 26.sizeof()/sizeof.cpp
@@ -12,4 +12,8 @@ int main()
 {
     Test test;                    // test is a variable of type Test
     cout << sizeof(test) << endl; // print size of test
+    
+    // Note that the size of the test variable is 8 bytes a/c to the compiler.
+    // It should have been 5 bytes a/c to us.
+    // Reason behind this is that the compiler allign the size of any variable to nearest multiple of 4.
 }
