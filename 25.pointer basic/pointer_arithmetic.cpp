3e38bd15381d36a17235bba348d4b5d7b4d0920e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-15 02:19:55 +0530

    demonstrate incrementation

--- 25.pointer basic/pointer_arithmetic.cpp
+++ 25.pointer basic/pointer_arithmetic.cpp
@@ -8,4 +8,8 @@ int main()
     int *ptr;
     ptr = x;
     cout << "Address of ptr = " << ptr << endl;
+    *ptr = 10;
+    cout << "value of ptr pointing to x[0] = " << *ptr << endl;
+    ptr++;
+    cout << "Address of ptr onn incrementation = " << ptr << endl;
 }
