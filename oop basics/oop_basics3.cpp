23beee5297d6e884ece3e30c4d5846d0ae3438b8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-16 14:23:12 +0530

    document the code

--- oop basics/oop_basics3.cpp
+++ oop basics/oop_basics3.cpp
@@ -2,27 +2,29 @@
 
 using namespace std;
 
-class time
+class time // create a class named time
 {
-    int hour;
-    int minute;
+    int hour;   // store hour
+    int minute; // store minute
 
-public:
-    void gettime()
+public:            // specy public data_members and member_functions
+    void gettime() // get time member function
     {
         cout << "Enter hour: ";
         cin >> hour;
         cout << "Enter minute: ";
         cin >> minute;
     }
-    void showtime()
+    void showtime() // show time member function
     {
         cout << "Time: " << hour << ":" << minute << endl;
     }
-    void sum(time t1, time t2)
+    void sum(time t1, time t2)  // sum member function
+    //We have passed two objects of class time as arguments to this member function
+    //This member function will add the time of two objects and return the sum of time
     {
-        hour = t1.hour + t2.hour;
-        minute = t1.minute + t2.minute;
+        hour = t1.hour + t2.hour; // add hour of two objects
+        minute = t1.minute + t2.minute;// add minute of two objects
         if (minute >= 60)
         {
             hour += 1;
@@ -33,10 +35,10 @@ public:
 
 int main()
 {
-    time t1, t2, t3;
-    t1.gettime();
-    t2.gettime();
-    t3.sum(t1, t2);
-    t3.showtime();
-    return 0;
+    time t1, t2, t3; // create t1, t2, t3 as objects of class time
+    t1.gettime();// call to gettime public member function
+    t2.gettime();// call to gettime public member function
+    t3.sum(t1, t2);// call to sum public member function
+    t3.showtime();// call to showtime public member function
+    return 0;// return 0
 }
