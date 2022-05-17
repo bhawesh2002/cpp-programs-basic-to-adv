9b8cfb3ad9f33a30c63652243286b030a08d3999
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-17 16:10:14 +0530

    demonstrate funnction overloading

--- 27.function overloading/function_overloading.cpp
+++ 27.function overloading/function_overloading.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+float sum(float c, float d)
+{
+    return c + d;
+}
+
 int main()
 {
+    int a = 10, b = 10;
+    float c = 10, d = 10;
+    cout << "sum(a,b) = " << sum(a, b) << endl;
+    cout << "sum(c,d)= " << sum(c, d) << endl;
     return 0;
 }
