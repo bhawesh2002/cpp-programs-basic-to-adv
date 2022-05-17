263ae4ea8bb77124af81d0b913a24a532ad31345
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-17 16:52:28 +0530

    print fibonacci series using class

---
+++ Obj And Class Examples/Fibonacci_series..cpp
@@ -0,0 +1,55 @@
+// Fibonacci series using classes;
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class series
+{
+    int terms;
+    int a;
+    int b;
+    int sum;
+
+public:
+    series()
+    {
+        a = 0;
+        b = 1;
+    }
+    int get_terms()
+    {
+        cout << "ENTER NO OF TERMS: ";
+        cin >> terms;
+        return terms;
+    }
+
+    void display_series()
+    {
+        cout << a << " + ";
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+
+    void display_sum()
+    {
+        cout << "\nSUM = " << sum << endl;
+    }
+};
+
+int main()
+{
+    series s1;
+    int terms;
+    terms = s1.get_terms();
+    for (int i = 0; i < terms; i++)
+    {
+        s1.display_series();
+    }
+    s1.display_sum();
+
+    cout << "PRESS ANY KEY TO END THE PROGRAM" << endl;
+    getch();
+    return 0;
+}
