e16c8b813cce57662fbe918b72150803a4107dd7
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-18 08:38:05 +0530

    code

--- Obj And Class Examples/maximum.cpp
+++ Obj And Class Examples/maximum.cpp
@@ -2,7 +2,36 @@
 
 using namespace std;
 
+class maximum
+{
+    int a, b, c;
+
+public:
+    void get_values()
+    {
+        cout << "Enter the value of a: ";
+        cin >> a;
+        cout << "Enter the value of b: ";
+        cin >> b;
+        cout << "Enter the value of c: ";
+        cin >> c;
+    }
+
+    void max_value()
+    {
+        if (a > b && a > c)
+            cout << a << " is the maximum of three" << endl;
+        else if (b > a && b > c)
+            cout << b << " is the maximum of three" << endl;
+        else
+            cout << c << " is the maximum of three" << endl;
+    }
+};
+
 int main()
 {
+    maximum m1;
+    m1.get_values();
+    m1.max_value();
     return 0;
 }
