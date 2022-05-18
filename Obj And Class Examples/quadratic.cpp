426dc038dcce13d39606f56c44f1e362d329296e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-18 08:51:08 +0530

    create class and add basiic member functiosn

--- Obj And Class Examples/quadratic.cpp
+++ Obj And Class Examples/quadratic.cpp
@@ -4,9 +4,30 @@ using namespace std;
 
 class quadratic
 {
+    int a, b, constant;
+    string quad;
+
+public:
+    void get_values()
+    {
+        cout << "Enter the value of a: ";
+        cin >> a;
+        cout << "Enter the value of b: ";
+        cin >> b;
+        cout << "Enter the value of constant: ";
+        cin >> constant;
+    }
+
+    void show_eqn()
+    {
+        cout << "The equation is: " << a << "x^2 + " << b << "x + " << constant << endl;
+    }
 };
 
 int main()
 {
+    quadratic q1;
+    q1.get_values();
+    q1.show_eqn();
     return 0;
 }
