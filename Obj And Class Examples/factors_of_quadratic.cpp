5b0bdcc2f933c07d9c0186d7bd01611ba5579837
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-20 18:01:35 +0530

    major changes

--- Obj And Class Examples/factors_of_quadratic.cpp
+++ Obj And Class Examples/factors_of_quadratic.cpp
@@ -4,11 +4,10 @@
 
 using namespace std; // using the standard namespace
 
-class eqn
+class factors
 {
-    int a, b, constant;
+    int a, b;
     int f1, f2;
-    string quad;
 
 public:
     void get_values()
@@ -17,36 +16,17 @@ public:
         cin >> a;
         cout << "Enter the value of b: ";
         cin >> b;
-        cout << "Enter the value of constant: ";
-        cin >> constant;
     }
-    void show_eqn()
+
+    void get_factors()
     {
-        cout << "The equation is: " << a << "x^2 + " << b << "x + " << constant << endl;
-    }
-    void factors()
-    {
-        for (int i = 1; i <= constant; i++)
-        {
-            for(int j = 2; j <= constant; j++)
-            {
-                if(i * j == constant)
-                {
-                    f1 = i;
-                    f2 = j;
-                    cout << "(f1,f2)" << "(" << f1 << "," << f2 << ")" << endl;
-                }
-            }
-        }
     }
 };
 
 int main()
 {
-    eqn q;
+    factors q;
     q.get_values();
-    q.show_eqn();
-    cout << "factors are: " << endl;
-    q.factors();
+    q.get_factors();
     return 0;
 }
