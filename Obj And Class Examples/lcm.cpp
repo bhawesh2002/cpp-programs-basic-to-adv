4cf1048420cfc6900238143797bf0926a56fbd24
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-20 18:51:45 +0530

    rename to lcm

---
+++ Obj And Class Examples/lcm.cpp
@@ -0,0 +1,41 @@
+#include <iostream> //basic input and output
+#include <cstring>  // for string manipulation provided by cstring
+#include <conio.h>  //console io
+
+using namespace std; // using the standard namespace
+
+class factors
+{
+    int a;
+
+public:
+    void get_values()
+    {
+        cout << "Enter the value of a: ";
+        cin >> a;
+    }
+
+    void get_factors()
+    {
+        cout << "factors of " << a << " are:" << endl;
+        for (int i = 1; i <= a; i++)
+        {
+            for (int j = 1; j <= a; j++)
+            {
+                if (i * j == a)
+                {
+                    cout << "(" << i << "," << j << "),";
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    factors q;
+    q.get_values();
+    q.get_factors();
+    getche();
+    return 0;
+}
