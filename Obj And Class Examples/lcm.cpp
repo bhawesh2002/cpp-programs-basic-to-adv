b681127b328fafb161c4787322d0e3e4859124f5
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-27 22:46:28 +0530

    determine the lcm of two numnbers

--- Obj And Class Examples/lcm.cpp
+++ Obj And Class Examples/lcm.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -16,10 +17,37 @@ public:
         cout << "Enter the value of b: ";
         cin >> b;
     }
+    void prime_factors()
+    {
+
+        int small, large;
+        if (a < b)
+        {
+            small = a;
+            large = b;
+        }
+        else
+        {
+            small = b;
+            large = a;
+        }
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                if ((small * i) == (large * j))
+                {
+                    cout << "The prime factor is " << (small * i) << endl;
+                    exit(0);
+                }
+            }
+        }
+    }
 };
 int main()
 {
     lcm l;
     l.get_values();
+    l.prime_factors();
     return 0;
 }
