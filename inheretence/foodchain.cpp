06a4a5a9dccdc97cc817a79628785d22e7f400d4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-05-30 19:51:59 +0530

    write the question

---
+++ inheretence/foodchain.cpp
@@ -0,0 +1,7 @@
+/*The food chain shows how each living thing gets its food. Some animals eat plants and
+some eat other animals to survive. Your project is to create first inheritance hierarchy
+models of all the food chains that you can think of starting with ‘forest’ as the base class.
+Then create a menu driven program which displays the food chains of the various animals
+as per the ‘food chain’ hierarchy. An example of food chain of a ‘Wolf’ is given below.
+Use inheritance concept where ever possible*/
+
