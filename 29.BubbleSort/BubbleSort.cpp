3c5ebc071b5a7187486383c09e5d59536e2f4874
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-01 08:47:59 +0530

    display the  numbers entered

--- 29.BubbleSort/BubbleSort.cpp
+++ 29.BubbleSort/BubbleSort.cpp
@@ -11,5 +11,11 @@ int main()
         cout << "Enter the " << i + 1 << " element: ";
         cin >> num[i];
     }
+    cout << "The numbers entered are:-\n";
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+    {
+        cout << num[i] << endl;
+    }
+
     return 0;
 }
