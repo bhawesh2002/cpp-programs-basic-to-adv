dc3923b9cfd52844e0da7904848fd0bf2c58169c
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-01 08:41:31 +0530

    correct the relational op used

--- 29.BubbleSort/BubbleSort.cpp
+++ 29.BubbleSort/BubbleSort.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int num[5] = {0};
     cout << "Enter numbers to fill the array:-\n";
-    for (int i = 0; i <= sizeof(num) / sizeof(int); i++)
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++)
     {
         cout << "Enter the " << i + 1 << "element: ";
         cin >> num[i];
