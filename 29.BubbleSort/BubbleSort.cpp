1726564a829a2b3830aa5a02127c813b84e7247a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-01 08:56:06 +0530

    remove display array  code block

--- 29.BubbleSort/BubbleSort.cpp
+++ 29.BubbleSort/BubbleSort.cpp
@@ -4,23 +4,18 @@ using namespace std;
 
 int main()
 {
-    int num[5] = {95,52,887,68,123};
+    int num[5] = {95, 52, 887, 68, 123};
 
     for (int i = 0; i < sizeof(num) / sizeof(num); i++)
     {
         for (int j = 1; j < (sizeof(num) / sizeof(num)); j++)
         {
-            if (num[j] > num[i])
+            if (num[j] < num[i])
             {
                 swap(num[i], num[j]);
+                cout << num[i] << endl;
             }
         }
-    }
-
-    cout << "The sorted array is:-\n";
-    for (int i = 0; i < sizeof(num) / sizeof(int); i++)
-    {
-        cout << num[i] << endl;
     }
     return 0;
 }
