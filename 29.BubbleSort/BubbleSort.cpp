b731df4f7583cf3dc8897a7ada33ddfd5badfe81
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-01 08:39:17 +0530

    use for loop to fill the array

--- 29.BubbleSort/BubbleSort.cpp
+++ 29.BubbleSort/BubbleSort.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int main()
 {
     int num[5] = {0};
+    cout << "Enter numbers to fill the array:-\n";
+    for (int i = 0; i <= sizeof(num) / sizeof(int); i++)
+    {
+        cout << "Enter the " << i << "element: ";
+        cin >> num[i];
+    }
 
     return 0;
 }
