c9535e438fbf885c3ea6aaab0552529e7fccefa0
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-01 09:03:31 +0530

    user filled array

--- 29.BubbleSort/BubbleSort.cpp
+++ 29.BubbleSort/BubbleSort.cpp
@@ -4,22 +4,53 @@ using namespace std;
 
 int main()
 {
-    int num[5] = {95, 52, 887, 68, 123};
+    int num[5] = {0};
 
+    cout << "Enter numbers to fill the array:-\n";
     for (int i = 0; i < sizeof(num) / sizeof(int); i++)
     {
-        for (int j = 0; j < (sizeof(num) / sizeof(int)) - 1; j++)
-        {
-            if (num[j] < num[i])
-            {
-                swap(num[i], num[j]);
-            }
-        }
+        cout << "Enter the " << i + 1 << " element: ";
+        cin >> num[i];
     }
     int choice;
     cout << "1)sort in ascending\n"
          << "2)sort in descending\n";
     cin >> choice;
+    switch (choice)
+    {
+    case 1:
+
+        for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+        {
+            for (int j = 0; j < (sizeof(num) / sizeof(int)) - 1; j++)
+            {
+                if (num[j] > num[i])
+                {
+                    swap(num[i], num[j]);
+                }
+            }
+        }
+        break;
+    case 2:
+
+        for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+        {
+            for (int j = 0; j < (sizeof(num) / sizeof(int)) - 1; j++)
+            {
+                if (num[j] < num[i])
+                {
+                    swap(num[i], num[j]);
+                }
+            }
+        }
+
+        break;
+    default:
+        cout << "Invalid Value" << endl;
+        cout << "Terminating " << endl;
+        exit(0);
+        break;
+    }
     cout << "The sorted array is:-\n";
     for (int i = 0; i < sizeof(num) / sizeof(int); i++)
     {
